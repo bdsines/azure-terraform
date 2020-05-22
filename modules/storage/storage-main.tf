d8c2d336c645e2fbeddf82eea303650bdf034810
bdsines <bdsines007@gmail.com>
2020-05-22 05:55:40 -0500

    storage account w/ nw rules

--- modules/storage/storage-main.tf
+++ modules/storage/storage-main.tf
@@ -3,15 +3,15 @@ data azurerm_resource_group "storagerg" {
   location=var.location
 }
 data azurerm_subnet "storagesubnet" {
-  resource_group_name  = azurerm_resource_group.storagerg.name
+  resource_group_name  = data.azurerm_resource_group.storagerg.name
   virtual_network_name  = var.vnet_name
   name = var.subnet_name
 }
 
 resource "azurerm_storage_account" "storageaccount" {
   name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.storagerg.name
-  location                 = azurerm_resource_group.storagerg.location
+  resource_group_name      = data.azurerm_resource_group.storagerg.name
+  location                 = data.azurerm_resource_group.storagerg.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
@@ -19,11 +19,11 @@ resource "azurerm_storage_account" "storageaccount" {
 }
 
 resource "azurerm_storage_account_network_rules" "test" {
-  resource_group_name  = azurerm_resource_group.storagerg.name
+  resource_group_name  = data.azurerm_resource_group.storagerg.name
   storage_account_name = azurerm_storage_account.storageaccount.name
 
   default_action             = "Allow"
   ip_rules                   = ["127.0.0.1"]
-  virtual_network_subnet_ids = [azurerm_subnet.storagesubnet.id]
+  virtual_network_subnet_ids = [data.azurerm_subnet.storagesubnet.id]
   bypass                     = ["Metrics"]
 }
